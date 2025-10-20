970618f5c60b68244a42e4825c980d1b4bb1de18
Francesco Pio Gaglione <francesco.gaglione.p@gmail.com>
2025-10-20 17:24:55 +0200

    fix update workspace, it is now possible to change category and task id if in break session

--- src/adapters/http/routes/ws/update_workspace.rs
+++ src/adapters/http/routes/ws/update_workspace.rs
@@ -1,20 +1,24 @@
 use tracing::debug;
 
 use crate::adapters::http::{
-    app_state::AppState, dto::ws_msg::update_workspace_ws::UpdateWorkspace,
+    app_state::AppState,
+    dto::{
+        common::session_type_enum::SessionTypeEnum, ws_msg::update_workspace_ws::UpdateWorkspace,
+    },
 };
 
 pub async fn update_workspace(
     message: &UpdateWorkspace,
     state: &AppState,
 ) -> Result<UpdateWorkspace, String> {
-    debug!("Starting session");
+    debug!("Updating workspace");
     let sessions_state = state.focus_session_state.clone();
-
     let mut state = sessions_state.write().await;
 
-    if state.current_session.is_some() {
-        return Err("Cannot change workspace if session is running".to_string());
+    if let Some(ref current_session) = state.current_session {
+        if current_session.session_type == SessionTypeEnum::Work {
+            return Err("Cannot change workspace if session is running".to_string());
+        }
     }
 
     state.workspace.category_id = message.category_id.clone();
