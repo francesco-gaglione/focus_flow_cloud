24a5a815b48714505755a0ce3f022322ad9c51ed
Francesco Pio Gaglione <francesco.gaglione.p@gmail.com>
2025-11-24 08:55:42 +0100

    removed logs

--- src/adapters/http/routes/category/get_categories_and_tasks_api.rs
+++ src/adapters/http/routes/category/get_categories_and_tasks_api.rs
@@ -38,10 +38,5 @@ pub async fn get_categories_and_tasks_api(
             .collect(),
     };
 
-    tracing::info!(
-        "Categories and tasks retrieved successfully: {:?}",
-        response
-    );
-
     Ok(Json(response))
 }
